3d0347e089fc54cd23ec4faa14001d08fce32798
Baejiu <jiumaker@naver.com>
2025-03-25 11:39:13 +0900

    fix: delete export

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     unoptimized: true,
   },
   compiler: {
     styledComponents: true,
   },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
